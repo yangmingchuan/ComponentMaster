e4fc39c1272f6ae48b8f80c977c2f3237e35ac44
yangmingchuan <18768880074@163.com>
2020-09-14 11:43:59 +0800

    【mc组件化】Adapter lib完善

---
+++ lib_common/src/main/java/com/ymc/common/adapter/base/BaseAdapter.kt
@@ -0,0 +1,13 @@
+package com.ymc.common.adapter.base
+
+import com.ymc.common.adapter.MultiItemTypeAdapter
+
+/**
+ * Author : ymc
+ * Date   : 2020/9/11  15:34
+ * Class  : BaseAdapter
+ */
+
+abstract class BaseAdapter : MultiItemTypeAdapter<T> {
+
+}
